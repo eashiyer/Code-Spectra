3c64b83ad1480445445549232039e1705bbd80fc
Phuong H <phuonghqh@gmail.com>
2015-11-11 22:10:04 +0700

    Try to fix email issue

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.1and1.com",
+  :address              => 'smtp.1and1.com',
   :port                 => 587,
-  :domain               => "mspectrumsolutions.com",
-  :user_name            => "admin@mspectrumsolutions.com",
-  :password             => "mailPass#1",
-  :authentication       => "plain",
+  :domain               => 'mspectrumsolutions.com',
+  :user_name            => 'admin@mspectrumsolutions.com',
+  :password             => 'mailPass#1',
+  :authentication       => :plain,
   :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
