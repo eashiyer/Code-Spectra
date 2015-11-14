e147d031b8d48cba1b68bdfa9eeccfc3cb6c9076
Phuong H <phuonghqh@gmail.com>
2015-11-14 13:41:46 +0700

    Replace new smtp server

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.1and1.com",
-  :port                 => "25",
-  :domain               => "1and1.com",
+  :address              => "smtp.perfora.net",
+  :port                 => "587",
+  # :domain               => "1and1.com",
   :user_name            => "phuong@mspectrumsolutions.com",
   :password             => "hochiminh",
   :authentication       => "plain",
@@ -18,7 +18,7 @@ ActionMailer::Base.smtp_settings = {
 #     :enable_starttls_auto => true
 # }
 
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.delivery_method = :sendmail
 # ActionMailer::Base.smtp_settings = {
 #     :address              => "smtp.1and1.com",
 #     :port                 => 587,
