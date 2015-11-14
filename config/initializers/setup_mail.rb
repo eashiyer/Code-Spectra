2b70a013595c736b22d0979267303620053d15dd
Phuong H <phuonghqh@gmail.com>
2015-11-14 11:34:46 +0700

    Change some configure to see

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.1and1.com",
-  :port                 => "587",
+  :port                 => "25",
   # :domain               => "1and1.com",
   :user_name            => "phuong@mspectrumsolutions.com",
   :password             => "hochiminh",
-  :authentication       => true,
+  :authentication       => :login,
   :enable_starttls_auto => true
 }
 
